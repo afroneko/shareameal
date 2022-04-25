23ff6e3ed241dbe50a56f4ef490cd102c86ffc41
AfroNeko <lisatyem@gmail.com>
2022-04-25 23:58:16 +0200

    UC201 till UC206 added

--- index.js
+++ index.js
@@ -36,47 +36,108 @@ app.get('/', (req, res) => {
   })
 })
 
-//UC-201 Register as a new user
-app.post('/api/movie', (req, res) => {
-  let movie = req.body
-  id++
-  movie = {
-    id,
-    ...movie,
-  }
-  console.log(movie)
-  database.push(movie)
-  res.status(201).json({
-    status: 201,
-    result: database,
-  })
-})
-
-//#UC-204 get single user by ID
-app.get('/api/movie/:movieId', (req, res, next) => {
-  const movieId = req.params.movieId
-  console.log(`Movie met ID ${movieId} gezocht`)
-  let movie = database.filter((item) => item.id == movieId)
-  if (movie.length > 0) {
-    console.log(movie)
-    res.status(200).json({
-      status: 200,
-      result: movie,
-    })
-  } else {
+//#UC-201 Register as a new user
+app.post('/api/user', (req, res) => {
+  let user = req.body
+  console.log(user)
+  let email = user.emailAdress
+  let userList = database.filter((item) => item.emailAdress == email)
+  if (userList.length > 0) {
     res.status(401).json({
       status: 401,
-      result: `Movie with ID ${movieId} not found`,
+      result: `Emailadress:  ${email} has already been used.`,
+    })
+  } else {
+    user = {
+      id,
+      ...user,
+    }
+    id++
+    database.push(user)
+    console.log(database)
+    res.status(201).json({
+      status: 201,
+      result: `User with email: ${email} is added.`,
     })
   }
 })
 
-//UC-202 get all users
-app.get('/api/movie', (req, res, next) => {
+//#UC-202 get all users
+app.get('/api/user', (req, res, next) => {
   res.status(200).json({
     status: 200,
     result: database,
   })
+})
+
+//#UC-203 Request personal user profile
+app.get('api/user/profile', (req, res, next) => {
+  res.status(501).json({
+    status: 501,
+    result: 'This service is not yet available.',
+  })
+})
+
+//#UC-204 get single user by ID
+app.get('/api/user/:userId', (req, res, next) => {
+  const userId = req.params.userId
+  console.log(`User met ID ${userId} gezocht`)
+  let user = database.filter((item) => item.id == userId)
+  if (user.length > 0) {
+    console.log(user)
+    res.status(200).json({
+      status: 200,
+      result: user,
+    })
+  } else {
+    res.status(404).json({
+      status: 404,
+      result: `User with ID ${userId} not found`,
+    })
+  }
+})
+
+//#UC-205 Update a single user
+app.put('api/user/:userID', (req, res, next) => {
+  const userID = req.params.userID
+  let userList = database.filter((item) => item.id == userID)
+  if (userList.length > 0) {
+    console.log(userList)
+    let user = req.body
+    user = {
+      id,
+      ...user,
+    }
+    database[database.indexOf(userList[0])] = user
+    res.status(201).json({
+      status: 201,
+      result: `User with ID ${userID} is updated`,
+    })
+  } else {
+    res.status(401).json({
+      status: 401,
+      result: `You are not authorized`,
+    })
+  }
+})
+
+//#UC-206 Delete a user
+app.delete('api/user/:userID', (req, res, next) => {
+  const userID = req.params.userID
+  let userList = database.filter((item) => item.id == userID)
+  if (userList.length > 0) {
+    console.log(userList)
+    database.splice(database.indexOf(userList[0]), 1)
+    res.status(201).json({
+      status: 201,
+      result: `User with ID ${userID} is deleted`,
+    })
+  } else {
+    res.status(401).json({
+      status: 401,
+      result: `You are not authorized`,
+    })
+  }
 })
 
 app.all('*', (req, res) => {
